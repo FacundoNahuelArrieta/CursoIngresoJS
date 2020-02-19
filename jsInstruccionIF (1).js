71aac6852a4e254bded75761e8be964e1b7e0372
FacundoNahuelArrieta <61198299+FacundoNahuelArrieta@users.noreply.github.com>
2020-02-19 11:09:59 -0300

    Add files via upload
    ejercicios if

---
+++ jsInstruccionIF (1).js
@@ -0,0 +1,13 @@
+function mostrar()
+{  var edad;
+
+    edad = document.getElementById("edad").value;
+
+    if (edad == 15) {
+        
+        alert("Niña Bonita");
+        
+    }
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionIF (7).js
@@ -0,0 +1,19 @@
+function mostrar()
+{
+//tomo la edad  
+var edad;
+var estado;
+
+edad = parseInt(document.getElementById("edad").value);
+
+estado = document.getElementById("estadoCivil").value;
+
+if(edad <18 && estado != soltero){
+
+      alert("Es muy pequeño para ser soltero");
+
+}
+	
+
+
+}//FIN DE LA FUNCIÓN
